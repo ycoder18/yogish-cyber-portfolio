fd0ff40b1ac0f7835b96e728e1d89e31ab2a8e4c
ycoder18 <112474104+ycoder18@users.noreply.github.com>
2025-10-22 05:27:09 +0530

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    // This plugin is REQUIRED to process Tailwind directives and generate CSS
+    'tailwindcss': {},
+    // This plugin adds necessary vendor prefixes (e.g., -webkit-) for compatibility
+    'autoprefixer': {},
+  },
+};
